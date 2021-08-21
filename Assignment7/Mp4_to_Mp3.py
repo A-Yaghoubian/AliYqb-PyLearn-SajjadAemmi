3b9129aa35e84b870532e7e7f7947325fdb31c86
Ali Yaghoubian <79134287+AliYqb@users.noreply.github.com>
2021-08-21 16:26:26 +0430

    Update Mp4_to_Mp3.py

--- Assignment7/Mp4_to_Mp3.py
+++ Assignment7/Mp4_to_Mp3.py
@@ -1,3 +1,3 @@
 from moviepy import editor
-video = editor.VideoFileClip('test.mp4') # Your MP4 Address
-video.audio.write_audiofile('test.mp3') # Your MP3 Address
+video = editor.VideoFileClip('E:\programming\PyLearn-SajjadAemmi\Assignment7\khande dar.mp4') # Your MP4 Address
+video.audio.write_audiofile('E:\programming\PyLearn-SajjadAemmi\Assignment7\khande dar.mp3') # Your MP3 Address
