80aa2b8c7a7c9133339dc60a3407bd8bcfef7921
Ali Yaghoubian <79134287+AliYqb@users.noreply.github.com>
2021-09-21 09:43:48 +0430

    Delete Assignmnet14/Platformer_game directory

--- Assignmnet14/Platformer_game/star.py
+++
@@ -1,9 +0,0 @@
-import arcade
-
-class Star(arcade.Sprite):
-    def __init__(self, w, h):
-        super().__init__(':resources:images/items/star.png')
-        self.center_x = w
-        self.center_y = h
-        self.width = 50
-        self.height = 50
