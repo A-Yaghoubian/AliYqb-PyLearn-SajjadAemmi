73c81a816a16f10a11e789c83f8a95e77b80fa2a
Ali Yaghoubian <79134287+AliYqb@users.noreply.github.com>
2021-09-24 19:34:38 +0330

    Delete Assignmnet14_2/Dino-googleChromeRunner directory

--- Assignmnet14_2/Dino-googleChromeRunner/ground.py
+++
@@ -1,8 +0,0 @@
-import arcade
-
-class Ground(arcade.Sprite):
-    def __init__(self, w, h):
-        super().__init__()
-        self.texture = arcade.load_texture('images\ground.png')
-        self.center_x = w // 4
-        self.center_y = h // 9
