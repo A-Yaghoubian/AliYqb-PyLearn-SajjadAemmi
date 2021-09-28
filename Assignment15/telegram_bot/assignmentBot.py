8e996c2fe6a36f7af8c36a64bc53228ca3c5c910
Ali Yaghoubian <79134287+AliYqb@users.noreply.github.com>
2021-09-28 08:56:01 +0330

    Update assignmentBot.py

--- Assignment15/telegram_bot/assignmentBot.py
+++ Assignment15/telegram_bot/assignmentBot.py
@@ -1,34 +1,32 @@
-from enum import Flag
 import random
 import telebot
 
-def hadsAdad(message):
-    answer_number = random.randint(0, 1000)
-    bot.send_message(message.chat.id, 'یک عدد بین ۰ تا ۱۰۰۰ بگو')
-    flag = 0
-    while True:
-        @bot.message_handler(func=lambda message: True)
-        def check_game(message):
-            if int(message.text) > answer_number:
-                bot.send_message(message.chat.id, 'کمترش کن')
-            elif int(message.text) < answer_number:
-                bot.send_message(message.chat.id, 'بیشترش کن')
-            elif int(message.text) == answer_number:
-                bot.send_message(message.chat.id, 'ماشالااااا خودشه\nپیداش کردی')
-                flag = 1
-        if flag == 1:
-            break
-
 bot = telebot.TeleBot("2019327355:AAF9JCQPiBsOQKrEivcNfGlAoFk5nWKQ0jc")
+game_flag = 0
+game_answer_number = random.randint(0, 1000)
 
-@bot.message_handler(['Start', 'start', 'شروع'])
+@bot.message_handler(commands=['Start', 'start', 'شروع'])
 def wellcome(message):
     first_name = message.from_user.first_name
     bot.reply_to(message, f'{first_name} خوش اومدی')
 
-@bot.message_handler(['Game', 'game', 'بازی'])
+@bot.message_handler(commands=['Game', 'game', 'بازی'])
 def play_game(message):
     bot.reply_to(message, 'بازی حدس عدد')
-    hadsAdad(message)
+    bot.send_message(message.chat.id, 'یک عدد بین ۰ تا ۱۰۰۰ بگو')
+    game_flag = 1
+    
+if game_flag == 1:
+    @bot.message_handler(func=lambda message: True)
+    def check_game(message):
+        while True:
+            if int(message.text) > game_answer_number:
+                bot.send_message(message.chat.id, 'کمترش کن')
+            elif int(message.text) < game_answer_number:
+                bot.send_message(message.chat.id, 'بیشترش کن')
+            elif int(message.text) == game_answer_number:
+                bot.send_message(message.chat.id, 'ماشالااااا خودشه\nپیداش کردی')
+                break
+        game_flag = 0
     
 bot.polling()
